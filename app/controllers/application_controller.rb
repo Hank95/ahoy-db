1e11b58630e43e9af4b8fcada8eb23ed1b48d27c
Henry <hhpendleton@gmail.com>
2021-09-03 15:52:47 -0400

    init

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
     include ActionController::Cookies
     before_action :authorize
+    skip_before_action :verify_authenticity_token
     
     def authorize
       @current_user = User.find_by(id: session[:user_id])
+      # byebug
       render json: {error:["Not authorized"]}, status: :unauthorized unless @current_user
     end
     
--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,21 @@
 class BookingsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+    wrap_parameters format: []
+
+    def index 
+        bookings = @current_user.bookings.all
+        render json: bookings
+    end
+
+    def create
+        booking = @current_user.bookings.create(booking_params)
+        render json: booking
+    end
+
+
+    private
+
+    def booking_params
+        params.permit(:date, :guests, :boat_id)
+    end
 end
