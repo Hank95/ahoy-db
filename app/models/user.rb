673e425a485ca6947a9693c00f942a39dd5fdc16
Henry <hhpendleton@gmail.com>
2021-09-08 14:59:38 -0400

    i keep tryign...

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ApplicationRecord
     # has_many :boats, through: :bookings
     has_many :reviews
 
+
+    validates :username, presence: true
+    validates :username, uniqueness: true
+    validates :username, length: { minimum: 4 }
+    validates :email, presence: true
+    validates :email, uniqueness: true
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
 end
